65beb572333315674f2b31071da0ce7062c23a23
Lucasbc47 <lucasbc47@gmail.com>
2025-09-14 22:02:18 -0300

    add: vetor data structure header file

---
+++ vetor/vetor.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int capacidade;
+    int ocupacao;
+    int *v;
+} t_vetor;
+
+void inicializa_vetor(t_vetor *, int);
+
+int insere_valor(int, t_vetor *);
+int remove_valor(int *, t_vetor *);
+
+int esta_cheio(t_vetor *);
+int esta_vazio(t_vetor *);
+
+void mostra_vetor(t_vetor *);
+void preenche_vetor(t_vetor *);
+void libera_memoria(t_vetor *);
+
+// sorting
+void bubble_sort(t_vetor *);
+void selection_sort(t_vetor *);
+void insertion_sort(t_vetor *);
